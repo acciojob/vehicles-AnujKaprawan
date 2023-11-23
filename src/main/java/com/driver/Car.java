84e1620f492c46a3430d497bd95cac43d30415a6
Anuj Kaprawan <134187534+AnujKaprawan@users.noreply.github.com>
2023-11-23 19:22:19 +0530

    Update Car.java

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -15,66 +15,10 @@ public class Car extends Vehicle {
         this.wheels = wheels;
         this.doors = doors;
         this.gears = gears;
-        this.currentGear = 1;
         this.isManual = isManual;
         this.type = type;
         this.seats = seats;
-    }
-
-    public int getWheels() {
-        return this.wheels;
-    }
-
-    public void setWheels(int wheels) {
-        this.wheels = wheels;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public int getDoors() {
-        return doors;
-    }
-
-    public void setDoors(int doors) {
-        this.doors = doors;
-    }
-
-    public int getGears() {
-        return gears;
-    }
-
-    public void setGears(int gears) {
-        this.gears = gears;
-    }
-
-    public boolean isManual() {
-        return isManual;
-    }
-
-    public void setManual(boolean manual) {
-        this.isManual = manual;
-    }
-
-    public int getCurrentGear() {
-        return currentGear;
-    }
-
-    public void setCurrentGear(int currentGear) {
-        this.currentGear = currentGear;
-    }
-
-    public int getSeats() {
-        return seats;
-    }
-
-    public void setSeats(int seats) {
-        this.seats = seats;
+        this.currentGear = 1;
     }
 
     public void changeGear(int newGear){
@@ -83,7 +27,35 @@ public class Car extends Vehicle {
     }
 
     public void changeSpeed(int newSpeed, int newDirection){
-        super.move(newSpeed,newDirection);
+        super.move(newSpeed, newDirection);
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
+    }
+
+    public int getDoors(){
+        return this.doors;
+    }
+
+    public int getGears(){
+        return this.gears;
+    }
+
+    public int getWheels(){
+        return this.wheels;
+    }
+
+    public boolean isManual(){
+        return this.isManual;
+    }
+
+    public String getType(){
+        return this.type;
+    }
+
+    public int getSeats(){
+        return this.seats;
+    }
+
+    public int getCurrentGear(){
+        return this.currentGear;
     }
 }
